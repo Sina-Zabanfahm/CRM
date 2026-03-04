e67eac9189c1e2a01291f844f4d3d0e09decf482
Sina Z <sinazabanfahm@gmail.com>
2026-03-04 18:49:37 -0500

    crawler4ai execution

---
+++ tests/crawl4ai_execution_test.py
@@ -0,0 +1,37 @@
+import uuid
+
+from src.executions.crawler.crawl4ai_exection import Crawl4AIExecution
+from src.states.execution_state import ExecutionState
+from src.states.artifact import Artifact
+
+
+def test_crawl4ai_execution():
+
+    state = ExecutionState()
+    run_id = "test_run"
+
+    url_artifact = Artifact[str](
+        id=str(uuid.uuid4()),
+        kind="text",
+        name="url",
+        content="https://github.com/Sina-Zabanfahm/Fin_AT"
+    )
+
+    state.artifacts[run_id] = {
+        "url": url_artifact
+    }
+
+    execution = Crawl4AIExecution()
+
+    outputs = execution.run(state, run_id)
+
+    for artifact in outputs:
+        print("Artifact ID:", artifact.id)
+        print("Kind:", artifact.kind)
+        print("Name:", artifact.name)
+        print("Content preview:", artifact.content[:500])
+
+    assert artifact is not None
+
+
+test_crawl4ai_execution()
